ced91a5dd02cddbff42847a573af27c8d20299e0
PasinduShavinda <pasindu.shv@gmail.com>
2021-09-21 19:21:26 +0530

    order crud functions done with search function

--- app/src/main/java/mad/example/teamdragons/MainActivity.java
+++ app/src/main/java/mad/example/teamdragons/MainActivity.java
@@ -2,13 +2,37 @@ package mad.example.teamdragons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnOrder = (Button) findViewById(R.id.btn_orderHome);
+
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecond();
+            }
+        });
+    }
+
+    // navigation
+
+    public void openSecond(){
+
+        Intent intent = new Intent(this, orderActivityFirst.class);
+
+        startActivity(intent);
+
     }
 }
